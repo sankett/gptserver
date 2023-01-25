2a3679d5ad67b0c826c2b847c7680a6e02ed7801
Sanket Terdal <Sanket.Terdal@blueconchtech.com>
2023-01-25 13:04:35 +0530

    First

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { Configuration, OpenAIApi } = require('openai');
+const dotenv = require('dotenv');
+dotenv.config();
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = express();
+const port = 3000;
+
+const openai = new OpenAIApi(configuration);
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/moderation', async (req, res) => {
+    const response = await openai.createModeration({
+        input: `Write a Job Description for a 6 years experienced Full Stack engineer with expertise in React.JS`,
+      });
+    const flagged = response.data.results[0].flagged;
+      
+    res.status(200).json({ flagged: flagged });
+});
+
+app.get('/short', async (req, res) => {
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `Write a Job Description for a 6 years experienced Full Stack engineer with expertise in React.JS`,
+        max_tokens: 150,
+        temperature: 0,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        stream: false,
+    });
+
+    const promptOutput = response.data.choices.pop(); 
+   
+    res.status(200).json({ output: promptOutput });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
